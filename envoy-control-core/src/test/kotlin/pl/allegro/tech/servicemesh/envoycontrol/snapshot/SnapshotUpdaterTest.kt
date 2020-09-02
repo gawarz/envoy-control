6972b694c696fbb21a7c2b79a8c1b1608aab915c
Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>
2020-09-02 15:20:36 +0200

    Create empty locality group for cluster where service doesnt exist to not send unnecessary EDS

--- envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/SnapshotUpdaterTest.kt
+++ envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/SnapshotUpdaterTest.kt
@@ -95,8 +95,8 @@ class SnapshotUpdaterTest {
         // groups are generated foreach element in SnapshotCache.groups(), so we need to initialize them
         val groups = listOf(
             AllServicesGroup(communicationMode = XDS), groupWithProxy, groupWithServiceName,
-                groupOf(services = serviceDependencies("existingService1")),
-                groupOf(services = serviceDependencies("existingService2"))
+            groupOf(services = serviceDependencies("existingService1")),
+            groupOf(services = serviceDependencies("existingService2"))
         )
         groups.forEach {
             cache.setSnapshot(it, uninitializedSnapshot)
@@ -206,8 +206,8 @@ class SnapshotUpdaterTest {
     fun `should not crash on bad snapshot generation`() {
         // given
         val servicesGroup = AllServicesGroup(
-                communicationMode = ADS,
-                serviceName = "example-service"
+            communicationMode = ADS,
+            serviceName = "example-service"
         )
         val cache = FailingMockCache()
         cache.setSnapshot(servicesGroup, null)
@@ -215,14 +215,14 @@ class SnapshotUpdaterTest {
 
         // when
         updater.start(
-                Flux.just(MultiClusterState.empty())
+            Flux.just(MultiClusterState.empty())
         ).blockFirst()
 
         // then
         val snapshot = cache.getSnapshot(servicesGroup)
         assertThat(snapshot).isEqualTo(null)
         assertThat(simpleMeterRegistry.find("snapshot-updater.services.example-service.updates.errors")
-                .counter()?.count()).isEqualTo(1.0)
+            .counter()?.count()).isEqualTo(1.0)
     }
 
     @Test
@@ -314,6 +314,73 @@ class SnapshotUpdaterTest {
     }
 
     @Test
+    fun `should not change EDS when remote doesn't have state of service`() {
+        // given
+        val cache = MockCache()
+        val allServicesGroup = AllServicesGroup(communicationMode = ADS)
+        val groups = listOf(allServicesGroup)
+        val updater = snapshotUpdater(
+            cache = cache,
+            properties = SnapshotProperties().apply {
+                stateSampleDuration = Duration.ZERO
+            },
+            groups = groups
+        )
+
+        val clusterLocal = ClusterState(
+            ServicesState(
+                serviceNameToInstances = mapOf(
+                    "service" to ServiceInstances("service", setOf()),
+                    "service2" to ServiceInstances("service2", setOf())
+                )
+            ),
+            Locality.LOCAL, "cluster"
+        ).toMultiClusterState()
+
+        val clusterRemote1 = ClusterState(
+            ServicesState(
+                serviceNameToInstances = mapOf(
+                    "service" to ServiceInstances("service", setOf()),
+                    "service2" to ServiceInstances("service2", setOf())
+                )
+            ),
+            Locality.REMOTE, "cluster2"
+        ).toMultiClusterState()
+
+        val clusterRemote2 = ClusterState(
+            ServicesState(
+                serviceNameToInstances = mapOf(
+                    "service" to ServiceInstances("service", setOf())
+                )
+            ),
+            Locality.REMOTE, "cluster2"
+        ).toMultiClusterState()
+
+        // when
+        val results = updater
+            .services(Flux
+                .just(
+                    clusterLocal,
+                    clusterRemote1)
+                .delayElements(Duration.ofMillis(10))
+            )
+            .collectList().block()!!
+
+        // when
+        val results2 = updater
+            .services(Flux
+                .just(
+                    clusterLocal,
+                    clusterRemote2)
+                .delayElements(Duration.ofMillis(10))
+            )
+            .collectList().block()!!
+
+        // then
+        assertThat(true).isEqualTo(true)
+    }
+
+    @Test
     fun `should not remove clusters`() {
         // given
         val cache = MockCache()
@@ -351,10 +418,10 @@ class SnapshotUpdaterTest {
 
         results[1].adsSnapshot!!
             .hasHttp2Cluster("service")
-            .hasEmptyEndpoints("service")
+            .hasAnEmptyEndpointInLocality("service", setOf("cluster"))
         results[1].xdsSnapshot!!
             .hasHttp2Cluster("service")
-            .hasEmptyEndpoints("service")
+            .hasAnEmptyEndpointInLocality("service", setOf("cluster"))
     }
 
     @Test
@@ -547,6 +614,15 @@ class SnapshotUpdaterTest {
         val cluster = this.clusters.resources()[clusterName]
         assertThat(cluster).isNotNull
         assertThat(cluster!!.hasHttp2ProtocolOptions()).isTrue()
+        return this
+    }
+
+    private fun GlobalSnapshot.hasAnEmptyEndpointInLocality(clusterName: String, zones: Set<String>):
+        GlobalSnapshot {
+        val endpoints = this.endpoints.resources()[clusterName]
+        assertThat(endpoints).isNotNull
+        assertThat(endpoints!!.endpointsList.map { it.locality.zone }.toSet()).isEqualTo(zones)
+        assertThat(endpoints!!.endpointsList.flatMap { it.lbEndpointsList }).isEmpty()
         return this
     }
 
